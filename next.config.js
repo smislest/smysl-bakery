e03abe456f417af2d02fe9f94b85a1c44ff98cfe
marokone <marokone@gmail.com>
2025-12-08 20:47:03 +0300

    SECURITY: Update Next.js to 16.0.7 for Vercel deploy

---
+++ next.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  }
+}
+module.exports = nextConfig;
