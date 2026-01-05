06e2bf03e30792c9432e81b788c66660812c9fe0
marokone <marokone@gmail.com>
2026-01-06 00:05:43 +0300

    Expand DOMPurify allowedTags for WYSIWYG content

--- app/components/SafeContent.tsx
+++ app/components/SafeContent.tsx
@@ -9,7 +9,23 @@ interface SafeContentProps {
 }
 
 export default function SafeContent({ content, className, style }: SafeContentProps) {
-  const cleanContent = DOMPurify.sanitize(content);
+  const cleanContent = DOMPurify.sanitize(content, {
+    ALLOWED_TAGS: [
+      'p', 'br', 'strong', 'b', 'em', 'i', 'u', 'a',
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+      'blockquote', 'pre', 'code',
+      'ul', 'ol', 'li',
+      'img', 'figure', 'figcaption',
+      'table', 'thead', 'tbody', 'tr', 'td', 'th',
+      'hr', 'div', 'span'
+    ],
+    ALLOWED_ATTR: [
+      'href', 'target', 'rel',
+      'src', 'alt', 'width', 'height',
+      'class', 'id', 'style',
+      'data-*'
+    ],
+  });
   
   return (
     <div
